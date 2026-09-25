cc99d075f836dc0e885f267130ddecd4ae179ef1
guada atim <guadiiatim1309@gmail.com>
2026-09-25 00:30:46 -0300

    templates modificados para mostrar desde la base de datos

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.shortcuts import render
+#from .models import Post
 
 
 class Post(models.Model):
