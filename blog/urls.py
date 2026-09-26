69a61cb7abba665d973d6191d022c3c7cfbdc3b8
guada atim <guadiiatim1309@gmail.com>
2026-09-25 23:20:54 -0300

    agrego vistas y plantilla para el detalle del post del blog y css

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 # asociamos una vista llamada post list a la url raiz
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
